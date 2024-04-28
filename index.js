3c01edc0859189a69cfb90bafc7ae4f870bdea60
Varun <varunguleria1700@gmail.com>
2024-04-28 23:39:13 +0530

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const geoip=require('geoip-lite')
+var app = express()
+app.get("/",function(request,response){
+const ip=request.socket.remoteAddress;
+var geo = geoip.lookup(ip);
+response.send({"ip":ip,"geo-loc":geo,"mssg":"hi there"})
+})
+app.listen(10000, function () {
+console.log("Started application on port %d", 10000)
+});
